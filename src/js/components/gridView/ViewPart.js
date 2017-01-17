d138f7917949320f54ea3a582f3f62339a84f034
fancn <fanyj@yikeer.com>
2017-01-17 16:49:44 +0800

    remove log

--- src/js/components/gridView/ViewPart.js
+++ src/js/components/gridView/ViewPart.js
@@ -2,9 +2,6 @@ import React from 'react';
 
 export default class viewPart extends React.Component {
     render() {
-
-        console.log(this.props.className);
-
         return (
             <div class={this.props.className}>
                 {this.props.index}
