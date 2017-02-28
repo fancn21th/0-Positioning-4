35e65ebc6484339a983cd0d512e51a2682a17276
fancn <fanyj@yikeer.com>
2017-02-28 23:43:34 +0800

    complete test cases for stuffed gap array method

--- src/js/lib/FormDesignerHelper.js
+++ src/js/lib/FormDesignerHelper.js
@@ -1,24 +1,27 @@
 export const getStuffedArray = (gapArray) => {
     const stuffedArray = []
-    let sum = 4, lastSum = sum
+    let sum = 0, lastSum = 0
 
     const stuffing = (stuff) => {
         stuffedArray.push(stuff)
     }
 
     gapArray.forEach((element, index, array)=> {
-        sum -= element
-        if(element == 4 && sum != 0){
-            sum = 4
-            stuffing(lastSum)
-        }else if(sum == 0){
-            sum = 4
-        }else if(sum < 0){
-            stuffing(-sum)
-            sum = 4
-        }
-        stuffedArray.push(element)
         lastSum = sum
+        sum += element
+
+        if(sum == 4){ // current element has no gap to next element
+            sum = 0
+        }else if(sum > 4){ // current element has gap to next element then fill it
+            stuffing(4 - lastSum)
+            if(element == 4){ // current element is equal to 4 then reset sum to 0
+                sum = 0
+            }else { // otherwise reset sum to current element
+                sum = element
+            }
+        }
+
+        stuffedArray.push(element)
     })
 
     return stuffedArray;
--- src/js/lib/FormDesignerHelper.test.js
+++ src/js/lib/FormDesignerHelper.test.js
@@ -42,6 +42,13 @@ test('turn [4,1,4] into [4,1,3,4]', () => {
     expect(result).toEqual(expected)
 })
 
+test('turn [1,3,4] into [1,3,4]', () => {
+    const expected = [1,3,4]
+    const result = getStuffedArray([1,3,4])
+
+    expect(result).toEqual(expected)
+})
+
 test('turn [4,4,4] into [4,4,4]', () => {
     const expected = [4,4,4]
     const result = getStuffedArray([4,4,4])
@@ -49,3 +56,23 @@ test('turn [4,4,4] into [4,4,4]', () => {
     expect(result).toEqual(expected)
 })
 
+test('turn [1,2,3,4,3,2,1] into [1,2,1,3,1,4,3,1,2,1]', () => {
+    const expected = [1,2,1,3,1,4,3,1,2,1]
+    const result = getStuffedArray([1,2,3,4,3,2,1])
+
+    expect(result).toEqual(expected)
+})
+
+test('turn [4,3,2,1,2,3,4] into [4,3,1,2,1,1,2,2,3,1,4]', () => {
+    const expected = [4,3,1,2,1,1,2,2,3,1,4]
+    const result = getStuffedArray([4,3,2,1,2,3,4])
+
+    expect(result).toEqual(expected)
+})
+
+test('turn [4,3,2,1,4,3,2,1] into [4,3,1,2,1,1,4,3,1,2,1]', () => {
+    const expected = [4,3,1,2,1,1,4,3,1,2,1]
+    const result = getStuffedArray([4,3,2,1,4,3,2,1])
+
+    expect(result).toEqual(expected)
+})
